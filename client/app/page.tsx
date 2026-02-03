f7e3be25de866829c1676e3d62efe86ff7f35168
Mayank Saini <mayanksaini0416@gmail.com>
2026-02-03 20:04:25 +0530

    problem grid component added to page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,12 +7,15 @@ import Testimonials from "@/components/sections/Testimonials";
 import FAQ from "@/components/sections/FAQ";
 import CTA from "@/components/sections/CTA";
 import Footer from "@/components/layout/Footer";
+import ProblemGrid from "@/components/sections/ProblemGrid";
+
 
 export default function LandingPage() {
   return (
     <main className="min-h-screen bg-white">
       <Navbar />
       <Hero />
+      <ProblemGrid />
       {/* <Features /> */}
       {/* <Stats /> */}
       <HowItWorks />
