f5f56c80e28a251204e6ca7c3218bd40ac6f6900
Mayank Saini <mayanksaini0416@gmail.com>
2026-02-11 10:42:04 +0530

    search route updated

--- server/src/controllers/search.controller.ts
+++ server/src/controllers/search.controller.ts
@@ -1,48 +1,3 @@
-// import { Request, Response } from "express";
-// import { Provider } from "../models/Provider";
-
-// export const searchProviders = async (req: Request, res: Response) => {
-//   try {
-//     const { lat, lng, skill, radius } = req.query;
-
-//     if (!lat || !lng) {
-//       return res.status(400).json({ error: "Location is required" });
-//     }
-
-//     const latitude = Number(lat);
-//     const longitude = Number(lng);
-//     const searchRadius = Number(radius) || 5000; // Default 5km in meters
-
-//     // Build the query
-//     let query: any = {
-//       location: {
-//         $near: {
-//           $geometry: {
-//             type: "Point",
-//             coordinates: [longitude, latitude] // MongoDB uses [Lng, Lat]
-//           },
-//           $maxDistance: searchRadius
-//         }
-//       }
-//     };
-
-//     // Filter by skill if provided (e.g., "Plumber")
-//     if (skill) {
-//       query.skills = { $in: [new RegExp(skill as string, 'i')] }; // Case-insensitive
-//     }
-
-//     const providers = await Provider.find(query).limit(20);
-
-//     res.json({
-//       count: providers.length,
-//       providers
-//     });
-
-//   } catch (error) {
-//     res.status(500).json({ error: "Server Error" });
-//   }
-// };
-
 
 import type {Request, Response} from "express";
 import Provider from "../models/Provider.js"
@@ -61,11 +16,11 @@ export const searchProviders = async(
         const providers = await Provider.find({
       service: String(service).toLowerCase(),
       pincode,
-      isActive: true
-    }).sort({ rating: -1 });
+    });
     res.json({
+      message: "success",
       count: providers.length,
-      providers
+      data :providers
     });
     }catch(error){
         res.status(500).json({error: "Server Error"})
